bb8e96ecb98e708f2cd5bfdf8995a0c91d0897df
Keerthana504 <130107749+Keerthana504@users.noreply.github.com>
2023-11-10 19:58:49 -0500

    10 Nov

--- tribute-page/index.js
+++ tribute-page/index.js
@@ -8,7 +8,6 @@ function makeApiRequest() {
     headers: { "X-Api-Key": apiKey },
     contentType: "application/json",
     success: function (result) {
-      //console.log(result[0].quote);
       //$("#thought").append(result[0].quote);
       let quote = result[0].quote + " --- " + result[0].author;
       $("#thought").text(quote);
@@ -23,25 +22,21 @@ makeApiRequest();
 
 // Schedule makeApiRequest to run every 5 minutes (300,000 milliseconds) using jQuery
 $(document).ready(function () {
-  setInterval(makeApiRequest, 30000);
+  setInterval(makeApiRequest, 20000);
   //$("#post-btn").css("display", "none");
   $("#post-btn").removeClass("display-btn");
 });
 
 let showContaniers = document.querySelectorAll(".show-replies");
-console.log(showContaniers);
 showContaniers.forEach((btn) => {
+  console.log("show replies");
   btn.addEventListener("click", (e) => {
-    console.log(e);
     let parentcontainer = e.target.closest(".comment-container");
-    console.log(parentcontainer);
     let id = parentcontainer.id;
-    console.log(id);
     if (id) {
       let childrenContainer = parentcontainer.querySelectorAll(
         `[dataset=${id}]`
       );
-      console.log(childrenContainer);
       childrenContainer.forEach((child) => {
         child.classList.toggle("opened");
       });
@@ -49,9 +44,57 @@ showContaniers.forEach((btn) => {
   });
 });
 
+let countLikes = document.getElementById("likes-first-comment");
+countLikes.addEventListener("click", (e) => {
+  console.log("once clicked");
+  let totalLikes = countLikes.value;
+  let countElement = `<span>. 1 </span>`;
+  if (value === 0) {
+    totalLikes++;
+    countLikes.value = totalLikes;
+  } else {
+    countLikes.innerHTML += countElement;
+  }
+});
+
 let userComment = document.getElementById("user-comment");
-// if (!document.getElementById("post-btn")) {
 userComment.addEventListener("keypress", (e) => {
   $("#post-btn").addClass("display-btn");
 });
-// }
+
+const USERID = {
+  name: "New Comment",
+  identity: null,
+  message: null,
+  date: null,
+};
+
+let commentAdd = document.querySelector("#post-btn");
+commentAdd.addEventListener("click", (e) => {
+  //adding a new comment to the section
+  console.log("Button clicked");
+  let comments = document.querySelector("#comments");
+  let newMessage = document.getElementById("user-comment");
+  USERID.message = newMessage.value;
+  USERID.date = new Date().toLocaleString();
+  let newComment = `<div class="comment-container opened" dataset="new-comment" id="new-comment-1">
+    <div class="comment-card">
+      <h4>${USERID.name}</h4>
+      <p> ${USERID.message}</p>
+      <div class="comment-card-footer">
+        <span class="date">${USERID.date}</span>
+        <div class="like"> 
+          <div>Like</div>
+          <div class="vertical-divider"></div>
+          <div class="show-replies">Reply</div> 
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  comments.insertAdjacentHTML("beforeend", newComment);
+  //comments.innerHTML += newComment;
+  newMessage.value = "";
+  $("#post-btn").removeClass("display-btn");
+  //commentAdd.classList.remove("display-btn");
+});
