13478f7269c26db6d1849a5e66f82062647657a6
Keerthana504 <130107749+Keerthana504@users.noreply.github.com>
2023-11-21 08:46:15 -0500

    21-Nov

--- JS-projects/project2.js
+++ JS-projects/project2.js
@@ -87,7 +87,7 @@ function stopWatch() {
 
 startStopBtn.addEventListener("click", function () {
   if (timerStatus === "stopped") {
-    timerInterval = window.setInterval(stopWatch, 1000);
+    timerInterval = window.setInterval(stopWatch, 100);
     document.getElementById("startStopBtn").innerHTML =
       '<i class="fa-solid fa-pause" id="pause"></i>';
     timerStatus = "started";
@@ -106,4 +106,50 @@ resetBtn.addEventListener("click", function () {
   hours = 0;
 
   document.getElementById("timer").innerHTML = "00:00:00";
+  document.getElementById("startStopBtn").innerHTML =
+    '<i class="fa-solid fa-play" id="play"></i>';
+  timerStatus = "stopped";
+});
+
+// Project 5 todo list
+
+const inputTask = document.getElementById("add-task");
+const addTask = document.getElementById("add-btn");
+const newTask = document.getElementById("task-container");
+
+addTask.addEventListener("click", function () {
+  const task = document.createElement("div");
+  task.classList.add("task");
+
+  const li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = `<i class="fa-solid fa-check" id="checked"></i>`;
+  checkButton.classList.add("checkTask");
+  task.appendChild(checkButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can" id="deleted"></i>`;
+  deleteButton.classList.add("deleteTask");
+  task.appendChild(deleteButton);
+
+  if (inputTask.value === "") {
+    alert("Please enter a Task");
+  } else {
+    newTask.appendChild(task);
+  }
+
+  checkButton.addEventListener("click", function () {
+    checkButton.parentElement.style.textDecoration = "line-through";
+  });
+  deleteButton.addEventListener("click", function (e) {
+    let target = e.target;
+    target.id === "deleted"
+      ? target.parentElement.parentElement.remove()
+      : target.parentElement.remove();
+  });
+
+  inputTask.value = "";
 });
