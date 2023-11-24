bb3ddd70025cf92f71fdd5cc698b5ddd5114c055
Keerthana504 <130107749+Keerthana504@users.noreply.github.com>
2023-11-24 18:15:44 -0500

    24 Nov

---
+++ css-basics/index.js
@@ -0,0 +1,9 @@
+console.log("hey");
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+
+right.addEventListener("click", function () {
+  console.log("clicked");
+});
